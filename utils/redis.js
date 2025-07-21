33cb693551681476c3611c987ad928004c996f3e
izzimars <israelmacaulay8@gmail.com>
2025-07-21 11:50:47 +0100

    adding reddis changes to the repository for reddis

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,17 @@
 const Redis = require("ioredis");
-const redis = new Redis(); // You can pass config options here
+
+const redis = new Redis({
+    host: "127.0.0.1", // default is localhost
+    port: 6379, // default Redis port
+    // password: "yourpassword", // if set
+});
+
+redis.on("connect", () => {
+    console.log("Redis connected");
+});
+
+redis.on("error", (err) => {
+    console.error("Redis error:", err);
+});
 
 module.exports = redis;
